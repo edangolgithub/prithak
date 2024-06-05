3260cd02bf49707f948123303cc36cb81906d66d
edangolgithub <dangolevan@gmail.com>
2024-06-05 09:02:20 +0545

    git cleam

--- frontend/src/test/AppList.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-it('renders a list with the correct number of items', () => {
-  render(<App />);
-  const listItems = screen.getAllByRole('listitem');
-  expect(listItems).toHaveLength(3); // Assuming there are 3 items in the list
-});
